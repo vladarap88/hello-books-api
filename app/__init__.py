5f62173e5b2016c1675299caa2d3b9a87f1ca46c
vlada <vladarap88@gmail.com>
2024-10-19 22:31:10 -0700

    learn 01 building api Read All Books Endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
+# __init__.py
+
 from flask import Flask
-from .routes.hello_world_routes import hello_world_bp
+from .routes.book_routes import books_bp
 
 
 def create_app():
     app = Flask(__name__)
 
     # Register Blueprints here
-    app.register_blueprint(hello_world_bp)
+    app.register_blueprint(books_bp)
 
     return app
