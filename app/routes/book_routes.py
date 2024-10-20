79949541670c031d91efe9c19c90fdb90310e25a
vlada <vladarap88@gmail.com>
2024-10-20 14:22:11 -0700

    Read One Book Endpoint

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -12,3 +12,15 @@ def get_all_books():
             {"id": book.id, "title": book.title, "description": book.description}
         )
     return books_response
+
+
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description,
+            }
